5f84b28bbc6054a5e82450e31548cc8fd9830042
ControltechCro <zklepic@controltech.hr>
2025-06-13 08:11:23 +0200

    Ponovno dodani file-ovi uz .gitignore primjenu

---
+++ ProjectFiles/NetSolution/Private/TypeConstants.cs
@@ -0,0 +1,54 @@
+using System;
+using UAManagedCore;
+
+//-------------------------------------------
+// WARNING: AUTO-GENERATED CODE, DO NOT EDIT!
+//-------------------------------------------
+
+namespace Demonstration
+{
+    public static class ObjectTypes
+    {
+        private static readonly int namespaceIndex = NamespaceMapProvider.GetNamespaceIndex("Demonstration");
+        public static readonly NodeId MainWindow = new NodeId(namespaceIndex, new Guid("01a7e8933afd7c95fe894b0859308138"));
+        public static readonly NodeId Parameters = new NodeId(namespaceIndex, new Guid("b85b1b61f5ff9da80d213a5ab0862e37"));
+        public static readonly NodeId Recipe = new NodeId(namespaceIndex, new Guid("a9d0ace2794bbbc7ed68171ce3ab6f55"));
+        public static readonly NodeId Configuration = new NodeId(namespaceIndex, new Guid("9cf154c923572624320f16c16fa657a3"));
+        public static readonly NodeId DateAndTimeWidget = new NodeId(namespaceIndex, new Guid("dfd608856ac9169edc3dd3847ce7f7bc"));
+        public static readonly NodeId Login = new NodeId(namespaceIndex, new Guid("ce814fddddd607c776a3b9a6e0b90c27"));
+        public static readonly NodeId FTRemoteAccessWidget = new NodeId(namespaceIndex, new Guid("41382c57dc616862c1af276ff6942abc"));
+        public static readonly NodeId PLC = new NodeId(namespaceIndex, new Guid("993b59924a12f899fc1ae269f7b02af8"));
+        public static readonly NodeId RequestAssistancePanel = new NodeId(namespaceIndex, new Guid("77122192dcccfaa39d1e78d7bd27e222"));
+        public static readonly NodeId NewConnectionCredentialsPanel = new NodeId(namespaceIndex, new Guid("d8a8bb0d6c0490587ebdf9c1eb42375e"));
+        public static readonly NodeId Dobot = new NodeId(namespaceIndex, new Guid("229683267164022ad24fa6f4e54b3f41"));
+        public static readonly NodeId Settings = new NodeId(namespaceIndex, new Guid("dbcc1657f2c288dd3f8171e79c2740c3"));
+        public static readonly NodeId Report = new NodeId(namespaceIndex, new Guid("5d5290f20f0919d8690a51610f71dc06"));
+        public static readonly NodeId Alarms = new NodeId(namespaceIndex, new Guid("f92dcaf50aadee1b3fca5fcc4d199c62"));
+        public static readonly NodeId Diag = new NodeId(namespaceIndex, new Guid("b1293153182ae653574d16bd8c6974cf"));
+        public static readonly NodeId DigitalInput = new NodeId(namespaceIndex, new Guid("c81edd64e5a8170f94681e5ac28fc228"));
+        public static readonly NodeId RequestAssistanceDisabledPanel = new NodeId(namespaceIndex, new Guid("f0afd51b3f49febd40b32153974e81b8"));
+        public static readonly NodeId DI_Config = new NodeId(namespaceIndex, new Guid("f413075d472826bc2ec756a6b133f603"));
+        public static readonly NodeId FTRemoteAccessWidgetDataObject = new NodeId(namespaceIndex, new Guid("07b10933e4ee4d4cbf84518e190ea9be"));
+        public static readonly NodeId NetworkSettingsWidget = new NodeId(namespaceIndex, new Guid("93cbf19dd68a72c543a41f03c3c2b7a1"));
+        public static readonly NodeId DeviceSettingsWidget = new NodeId(namespaceIndex, new Guid("343e9a553a1648e3de95b91f6d877b43"));
+        public static readonly NodeId LoginForm1 = new NodeId(namespaceIndex, new Guid("bb7394fbc2c52a07303154aaae41cdfd"));
+        public static readonly NodeId LAN = new NodeId(namespaceIndex, new Guid("41f433c486aa6dfdb14c2c47570eea1a"));
+        public static readonly NodeId WAN = new NodeId(namespaceIndex, new Guid("d17d8ab3d7f8047c35a02cbe0b7248c6"));
+        public static readonly NodeId Product_Information = new NodeId(namespaceIndex, new Guid("548a589cc1532b072d9d24316f7b6090"));
+        public static readonly NodeId Diagnostics = new NodeId(namespaceIndex, new Guid("632709bc0ca708c922da2f68cb20eb09"));
+        public static readonly NodeId OAuth2Login = new NodeId(namespaceIndex, new Guid("68099af806a871bc69c6e167f51f9755"));
+        public static readonly NodeId ConnectionPanel = new NodeId(namespaceIndex, new Guid("e348b14dbc7035688714c7ced8a72128"));
+        public static readonly NodeId AcceptDenyIncomingRequestPanel = new NodeId(namespaceIndex, new Guid("c05a81edcb91fa074772f19543ea1b38"));
+        public static readonly NodeId RequestAssistanceEnabledPanel = new NodeId(namespaceIndex, new Guid("dc2c41b238ed9ce9aa4f98d28d04b528"));
+        public static readonly NodeId EmptyPanel = new NodeId(namespaceIndex, new Guid("323ecbd80fe0dccc66307959b261f54a"));
+        public static readonly NodeId LoginForm = new NodeId(namespaceIndex, new Guid("d0fba00ff93f064d6e1cb546eea27209"));
+        public static readonly NodeId LoginPasswordExpiredDialog = new NodeId(namespaceIndex, new Guid("53b1bd1b17b4de66926108a7dfdf143e"));
+        public static readonly NodeId LoginChangePasswordForm = new NodeId(namespaceIndex, new Guid("fc0d46c3cf6fc6f7faace8fa13bde2b5"));
+        public static readonly NodeId Logout = new NodeId(namespaceIndex, new Guid("2eebfd3def044e747aa57952fc13c70d"));
+    }
+
+    public static class VariableTypes
+    {
+        private static readonly int namespaceIndex = NamespaceMapProvider.GetNamespaceIndex("Demonstration");
+    }
+}
---
+++ ProjectFiles/NetSolution/Private/UITypeDefinitions.cs
@@ -0,0 +1,176 @@
+using System;
+using UAManagedCore;
+
+//-------------------------------------------
+// WARNING: AUTO-GENERATED CODE, DO NOT EDIT!
+//-------------------------------------------
+
+[MapType(NamespaceUri = "Demonstration", Guid = "01a7e8933afd7c95fe894b0859308138")]
+public class MainWindow : FTOptix.UI.Window
+{
+}
+
+[MapType(NamespaceUri = "Demonstration", Guid = "b85b1b61f5ff9da80d213a5ab0862e37")]
+public class Parameters : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "Demonstration", Guid = "a9d0ace2794bbbc7ed68171ce3ab6f55")]
+public class Recipe : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "Demonstration", Guid = "9cf154c923572624320f16c16fa657a3")]
+public class Configuration : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "Demonstration", Guid = "dfd608856ac9169edc3dd3847ce7f7bc")]
+public class DateAndTimeWidget : FTOptix.UI.Screen
+{
+}
+
+[MapType(NamespaceUri = "Demonstration", Guid = "ce814fddddd607c776a3b9a6e0b90c27")]
+public class Login : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "Demonstration", Guid = "41382c57dc616862c1af276ff6942abc")]
+public class FTRemoteAccessWidget : FTOptix.UI.Screen
+{
+}
+
+[MapType(NamespaceUri = "Demonstration", Guid = "993b59924a12f899fc1ae269f7b02af8")]
+public class PLC : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "Demonstration", Guid = "77122192dcccfaa39d1e78d7bd27e222")]
+public class RequestAssistancePanel : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "Demonstration", Guid = "d8a8bb0d6c0490587ebdf9c1eb42375e")]
+public class NewConnectionCredentialsPanel : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "Demonstration", Guid = "229683267164022ad24fa6f4e54b3f41")]
+public class Dobot : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "Demonstration", Guid = "dbcc1657f2c288dd3f8171e79c2740c3")]
+public class Settings : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "Demonstration", Guid = "5d5290f20f0919d8690a51610f71dc06")]
+public class Report : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "Demonstration", Guid = "f92dcaf50aadee1b3fca5fcc4d199c62")]
+public class Alarms : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "Demonstration", Guid = "b1293153182ae653574d16bd8c6974cf")]
+public class Diag : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "Demonstration", Guid = "c81edd64e5a8170f94681e5ac28fc228")]
+public class DigitalInput : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "Demonstration", Guid = "f0afd51b3f49febd40b32153974e81b8")]
+public class RequestAssistanceDisabledPanel : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "Demonstration", Guid = "f413075d472826bc2ec756a6b133f603")]
+public class DI_Config : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "Demonstration", Guid = "93cbf19dd68a72c543a41f03c3c2b7a1")]
+public class NetworkSettingsWidget : FTOptix.UI.Screen
+{
+}
+
+[MapType(NamespaceUri = "Demonstration", Guid = "343e9a553a1648e3de95b91f6d877b43")]
+public class DeviceSettingsWidget : FTOptix.UI.Screen
+{
+}
+
+[MapType(NamespaceUri = "Demonstration", Guid = "bb7394fbc2c52a07303154aaae41cdfd")]
+public class LoginForm1 : LoginForm
+{
+}
+
+[MapType(NamespaceUri = "Demonstration", Guid = "41f433c486aa6dfdb14c2c47570eea1a")]
+public class LAN : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "Demonstration", Guid = "d17d8ab3d7f8047c35a02cbe0b7248c6")]
+public class WAN : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "Demonstration", Guid = "548a589cc1532b072d9d24316f7b6090")]
+public class Product_Information : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "Demonstration", Guid = "632709bc0ca708c922da2f68cb20eb09")]
+public class Diagnostics : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "Demonstration", Guid = "68099af806a871bc69c6e167f51f9755")]
+public class OAuth2Login : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "Demonstration", Guid = "e348b14dbc7035688714c7ced8a72128")]
+public class ConnectionPanel : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "Demonstration", Guid = "c05a81edcb91fa074772f19543ea1b38")]
+public class AcceptDenyIncomingRequestPanel : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "Demonstration", Guid = "dc2c41b238ed9ce9aa4f98d28d04b528")]
+public class RequestAssistanceEnabledPanel : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "Demonstration", Guid = "323ecbd80fe0dccc66307959b261f54a")]
+public class EmptyPanel : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "Demonstration", Guid = "d0fba00ff93f064d6e1cb546eea27209")]
+public class LoginForm : FTOptix.UI.PanelLoader
+{
+}
+
+[MapType(NamespaceUri = "Demonstration", Guid = "53b1bd1b17b4de66926108a7dfdf143e")]
+public class LoginPasswordExpiredDialog : FTOptix.UI.Dialog
+{
+}
+
+[MapType(NamespaceUri = "Demonstration", Guid = "fc0d46c3cf6fc6f7faace8fa13bde2b5")]
+public class LoginChangePasswordForm : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "Demonstration", Guid = "2eebfd3def044e747aa57952fc13c70d")]
+public class Logout : FTOptix.UI.Panel
+{
+}
