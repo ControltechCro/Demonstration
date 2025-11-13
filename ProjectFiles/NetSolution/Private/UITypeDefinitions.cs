2f0b1b0e5c5c0a331c09601928f16af2c37fc7cf
ControltechCro <zklepic@controltech.hr>
2025-11-13 07:07:02 +0100

    Add UI session and allowed sources to UI.yaml
    Increased MaxNumberOfConnections to 3 and added AllowedRemoteSources and Session node to UI.yaml. Updated type mapping order and NodeId constants in TypeConstants.cs and UITypeDefinitions.cs to match new UI structure. Adjusted references to older FactoryTalk Optix Studio modules for compatibility.

--- ProjectFiles/NetSolution/Private/UITypeDefinitions.cs
+++ ProjectFiles/NetSolution/Private/UITypeDefinitions.cs
@@ -5,68 +5,18 @@ using UAManagedCore;
 // WARNING: AUTO-GENERATED CODE, DO NOT EDIT!
 //-------------------------------------------
 
-[MapType(NamespaceUri = "Demonstration", Guid = "01a7e8933afd7c95fe894b0859308138")]
-public class MainWindow : FTOptix.UI.Window
-{
-}
-
-[MapType(NamespaceUri = "Demonstration", Guid = "b85b1b61f5ff9da80d213a5ab0862e37")]
-public class Plant : FTOptix.UI.Panel
-{
-}
-
-[MapType(NamespaceUri = "Demonstration", Guid = "a9d0ace2794bbbc7ed68171ce3ab6f55")]
-public class Recipe : FTOptix.UI.Panel
-{
-}
-
-[MapType(NamespaceUri = "Demonstration", Guid = "9cf154c923572624320f16c16fa657a3")]
-public class Configuration : FTOptix.UI.Panel
-{
-}
-
-[MapType(NamespaceUri = "Demonstration", Guid = "dfd608856ac9169edc3dd3847ce7f7bc")]
-public class DateAndTimeWidget : FTOptix.UI.Screen
-{
-}
-
-[MapType(NamespaceUri = "Demonstration", Guid = "ce814fddddd607c776a3b9a6e0b90c27")]
-public class Login : FTOptix.UI.Panel
-{
-}
-
-[MapType(NamespaceUri = "Demonstration", Guid = "1ead3f247fccea9e058749e6dcb2bc5c")]
-public class MotorStatus : FTOptix.UI.Panel
-{
-}
-
-[MapType(NamespaceUri = "Demonstration", Guid = "41382c57dc616862c1af276ff6942abc")]
-public class FTRemoteAccessWidget : FTOptix.UI.Screen
-{
-}
-
-[MapType(NamespaceUri = "Demonstration", Guid = "993b59924a12f899fc1ae269f7b02af8")]
-public class PLC : FTOptix.UI.Panel
-{
-}
-
 [MapType(NamespaceUri = "Demonstration", Guid = "77122192dcccfaa39d1e78d7bd27e222")]
 public class RequestAssistancePanel : FTOptix.UI.Panel
 {
 }
 
-[MapType(NamespaceUri = "Demonstration", Guid = "d8a8bb0d6c0490587ebdf9c1eb42375e")]
-public class NewConnectionCredentialsPanel : FTOptix.UI.Panel
+[MapType(NamespaceUri = "Demonstration", Guid = "548a589cc1532b072d9d24316f7b6090")]
+public class Product_Information : FTOptix.UI.Panel
 {
 }
 
-[MapType(NamespaceUri = "Demonstration", Guid = "229683267164022ad24fa6f4e54b3f41")]
-public class Dobot : FTOptix.UI.Panel
-{
-}
-
-[MapType(NamespaceUri = "Demonstration", Guid = "6db2e72c36ca71bcaf56bd214048b186")]
-public class DI_Config : FTOptix.UI.Dialog
+[MapType(NamespaceUri = "Demonstration", Guid = "01a7e8933afd7c95fe894b0859308138")]
+public class MainWindow : FTOptix.UI.Window
 {
 }
 
@@ -75,8 +25,8 @@ public class Settings : FTOptix.UI.Panel
 {
 }
 
-[MapType(NamespaceUri = "Demonstration", Guid = "5d5290f20f0919d8690a51610f71dc06")]
-public class Manual : FTOptix.UI.Panel
+[MapType(NamespaceUri = "Demonstration", Guid = "c81edd64e5a8170f94681e5ac28fc228")]
+public class DigitalInput : FTOptix.UI.Panel
 {
 }
 
@@ -85,23 +35,58 @@ public class Alarms : FTOptix.UI.Panel
 {
 }
 
-[MapType(NamespaceUri = "Demonstration", Guid = "b1293153182ae653574d16bd8c6974cf")]
-public class Report : FTOptix.UI.Panel
+[MapType(NamespaceUri = "Demonstration", Guid = "ce924a1d8828e46e6d3b2be12228f5a3")]
+public class FqMotor : FTOptix.UI.Panel
 {
 }
 
-[MapType(NamespaceUri = "Demonstration", Guid = "c81edd64e5a8170f94681e5ac28fc228")]
-public class DigitalInput : FTOptix.UI.Panel
+[MapType(NamespaceUri = "Demonstration", Guid = "dfd608856ac9169edc3dd3847ce7f7bc")]
+public class DateAndTimeWidget : FTOptix.UI.Screen
 {
 }
 
-[MapType(NamespaceUri = "Demonstration", Guid = "d0fba00ff93f064d6e1cb546eea27209")]
-public class LoginForm : FTOptix.UI.PanelLoader
+[MapType(NamespaceUri = "Demonstration", Guid = "229683267164022ad24fa6f4e54b3f41")]
+public class Dobot : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "Demonstration", Guid = "ce814fddddd607c776a3b9a6e0b90c27")]
+public class Login : FTOptix.UI.Panel
 {
 }
 
 [MapType(NamespaceUri = "Demonstration", Guid = "a897ba0fc448d2db790166dd674db06a")]
 public class TankStorage : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "Demonstration", Guid = "993b59924a12f899fc1ae269f7b02af8")]
+public class PLC : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "Demonstration", Guid = "41382c57dc616862c1af276ff6942abc")]
+public class FTRemoteAccessWidget : FTOptix.UI.Screen
+{
+}
+
+[MapType(NamespaceUri = "Demonstration", Guid = "a9d0ace2794bbbc7ed68171ce3ab6f55")]
+public class Recipe : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "Demonstration", Guid = "b85b1b61f5ff9da80d213a5ab0862e37")]
+public class Plant : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "Demonstration", Guid = "b1293153182ae653574d16bd8c6974cf")]
+public class Report : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "Demonstration", Guid = "5d5290f20f0919d8690a51610f71dc06")]
+public class Manual : FTOptix.UI.Panel
 {
 }
 
@@ -115,13 +100,8 @@ public class MonoValve : FTOptix.UI.Panel
 {
 }
 
-[MapType(NamespaceUri = "Demonstration", Guid = "ce924a1d8828e46e6d3b2be12228f5a3")]
-public class FqMotor : FTOptix.UI.Panel
-{
-}
-
-[MapType(NamespaceUri = "Demonstration", Guid = "c05a81edcb91fa074772f19543ea1b38")]
-public class AcceptDenyIncomingRequestPanel : FTOptix.UI.Panel
+[MapType(NamespaceUri = "Demonstration", Guid = "6db2e72c36ca71bcaf56bd214048b186")]
+public class DI_Config : FTOptix.UI.Dialog
 {
 }
 
@@ -130,18 +110,8 @@ public class DI_Config2 : FTOptix.UI.Dialog
 {
 }
 
-[MapType(NamespaceUri = "Demonstration", Guid = "548a589cc1532b072d9d24316f7b6090")]
-public class Product_Information : FTOptix.UI.Panel
-{
-}
-
 [MapType(NamespaceUri = "Demonstration", Guid = "a614487da8fe5a9d6dcd8a6eca671f80")]
 public class MotorPopup : FTOptix.UI.Dialog
-{
-}
-
-[MapType(NamespaceUri = "Demonstration", Guid = "2eebfd3def044e747aa57952fc13c70d")]
-public class Logout : FTOptix.UI.Panel
 {
 }
 
@@ -155,8 +125,18 @@ public class DeviceSettingsWidget : FTOptix.UI.Screen
 {
 }
 
+[MapType(NamespaceUri = "Demonstration", Guid = "e348b14dbc7035688714c7ced8a72128")]
+public class ConnectionPanel : FTOptix.UI.Panel
+{
+}
+
 [MapType(NamespaceUri = "Demonstration", Guid = "bb7394fbc2c52a07303154aaae41cdfd")]
 public class LoginForm1 : LoginForm
+{
+}
+
+[MapType(NamespaceUri = "Demonstration", Guid = "59994c979a1c71c98248e0be271372c7")]
+public class MotorParam : FTOptix.UI.Panel
 {
 }
 
@@ -165,8 +145,13 @@ public class MotorHome : FTOptix.UI.Panel
 {
 }
 
-[MapType(NamespaceUri = "Demonstration", Guid = "59994c979a1c71c98248e0be271372c7")]
-public class MotorParam : FTOptix.UI.Panel
+[MapType(NamespaceUri = "Demonstration", Guid = "fc0d46c3cf6fc6f7faace8fa13bde2b5")]
+public class LoginChangePasswordForm : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "Demonstration", Guid = "1ead3f247fccea9e058749e6dcb2bc5c")]
+public class MotorStatus : FTOptix.UI.Panel
 {
 }
 
@@ -185,18 +170,18 @@ public class WAN : FTOptix.UI.Panel
 {
 }
 
+[MapType(NamespaceUri = "Demonstration", Guid = "9cf154c923572624320f16c16fa657a3")]
+public class Configuration : FTOptix.UI.Panel
+{
+}
+
 [MapType(NamespaceUri = "Demonstration", Guid = "632709bc0ca708c922da2f68cb20eb09")]
 public class Diagnostics : FTOptix.UI.Panel
 {
 }
 
-[MapType(NamespaceUri = "Demonstration", Guid = "68099af806a871bc69c6e167f51f9755")]
-public class OAuth2Login : FTOptix.UI.Panel
-{
-}
-
-[MapType(NamespaceUri = "Demonstration", Guid = "e348b14dbc7035688714c7ced8a72128")]
-public class ConnectionPanel : FTOptix.UI.Panel
+[MapType(NamespaceUri = "Demonstration", Guid = "c05a81edcb91fa074772f19543ea1b38")]
+public class AcceptDenyIncomingRequestPanel : FTOptix.UI.Panel
 {
 }
 
@@ -205,8 +190,18 @@ public class RequestAssistanceEnabledPanel : FTOptix.UI.Panel
 {
 }
 
+[MapType(NamespaceUri = "Demonstration", Guid = "d8a8bb0d6c0490587ebdf9c1eb42375e")]
+public class NewConnectionCredentialsPanel : FTOptix.UI.Panel
+{
+}
+
 [MapType(NamespaceUri = "Demonstration", Guid = "f0afd51b3f49febd40b32153974e81b8")]
 public class RequestAssistanceDisabledPanel : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "Demonstration", Guid = "d0fba00ff93f064d6e1cb546eea27209")]
+public class LoginForm : FTOptix.UI.PanelLoader
 {
 }
 
@@ -220,7 +215,12 @@ public class LoginPasswordExpiredDialog : FTOptix.UI.Dialog
 {
 }
 
-[MapType(NamespaceUri = "Demonstration", Guid = "fc0d46c3cf6fc6f7faace8fa13bde2b5")]
-public class LoginChangePasswordForm : FTOptix.UI.Panel
+[MapType(NamespaceUri = "Demonstration", Guid = "2eebfd3def044e747aa57952fc13c70d")]
+public class Logout : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "Demonstration", Guid = "68099af806a871bc69c6e167f51f9755")]
+public class OAuth2Login : FTOptix.UI.Panel
 {
 }
